f03b377cbb601ab00f63e4de22415301aff8239c
darivera <darivera@gtc.com.gt>
2025-03-26 15:47:57 -0600

    registro de Clases

--- src/main/java/org/Selenium.java
+++ src/main/java/org/Selenium.java
@@ -1,4 +1,27 @@
-package java.org;
+package org;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Selenium {
+    protected WebDriver driver;
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    protected void executeJavaScript(String script, Object... args) {
+        if (driver instanceof JavascriptExecutor) {
+            ((JavascriptExecutor) driver).executeScript(script, args);
+        }
+    }
 }
--- src/test/java/PruebaTec/SeleniumTec.java
+++ src/test/java/PruebaTec/SeleniumTec.java
@@ -1,4 +1,64 @@
 package PruebaTec;
 
-public class SeleniumTec {
+import org.Selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.apache.commons.io.FileUtils;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public class SeleniumTec extends Selenium {
+
+    @BeforeClass
+    public void setUp() {
+        super.setUp(); // Llama al método setUp de la clase base
+    }
+
+    @Test
+    public void navigateAndCaptureScreenshots() throws IOException {
+        driver.get("https://www.selenium.dev/documentation/");
+        String[] buttons = {
+            "Documentation", "Overview", "WebDriver", "Selenium Manager", 
+            "Grid", "IE Driver Server", "IDE", "Test Practices", "Legacy", "About"
+        };
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        for (String button : buttons) {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(button)));
+            highlightElement(element);
+            element.click();
+            takeScreenshot(button);
+            driver.navigate().back();
+        }
+    }
+
+    private void highlightElement(WebElement element) {
+        String originalStyle = element.getAttribute("style");
+        executeJavaScript("arguments[0].setAttribute('style', arguments[1]);", element, "border: 2px solid red; border-style: dashed;");
+        // Restore original style after some time
+        executeJavaScript("arguments[0].setAttribute('style', arguments[1]);", element, originalStyle);
+    }
+
+    private void takeScreenshot(String buttonName) throws IOException {
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File outputDir = new File("screenshots");
+        FileUtils.copyFile(screenshot, new File(outputDir, buttonName + "_screenshot.png"));
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+        super.tearDown(); // Llama al método tearDown de la clase base
+    }
 }
